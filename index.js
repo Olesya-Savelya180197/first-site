0926d60ff0c9a97fb15b174f5ef791e1f331573d
Olesya Saveleva <114917006+Olesya-Savelya180197@users.noreply.github.com>
2023-06-26 15:37:16 +0700

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+        output.innerHTML = 'Ваня, я тебя люблю😘';
+})
